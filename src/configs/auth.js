ae1884c26be2b1a00888a1d5f6e768f8e01758ec
Jonatas_Amorim <jonatas231997@live.com>
2023-03-14 22:04:20 -0300

    Update auth.js

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,3 +1,4 @@
+//neste arquivo estamos gerando nosso jwt na aplicação, com sua key e o tempo que expira a autenticação
 module.exports = {
   jwt: {
     secret: process.env.AUTH_SECRET || "default",
